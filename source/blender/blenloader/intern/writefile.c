8f3e4c8625fbd67d2162b180dd9f6e4dd16940b0
Antonio Vazquez <blendergit@gmail.com>
2020-02-11 11:58:20 +0100

    GPencil: Fix typo error in struct name for write
    This produced errors loading the mask list.

--- source/blender/blenloader/intern/writefile.c
+++ source/blender/blenloader/intern/writefile.c
@@ -2728,7 +2728,7 @@ static void write_gpencil(WriteData *wd, bGPdata *gpd)
     writelist(wd, DATA, bGPDlayer, &gpd->layers);
     LISTBASE_FOREACH (bGPDlayer *, gpl, &gpd->layers) {
       /* Write mask list. */
-      writelist(wd, DATA, bGPDframe, &gpl->mask_layers);
+      writelist(wd, DATA, bGPDlayer_Mask, &gpl->mask_layers);
       /* write this layer's frames to file */
       writelist(wd, DATA, bGPDframe, &gpl->frames);
       LISTBASE_FOREACH (bGPDframe *, gpf, &gpl->frames) {
