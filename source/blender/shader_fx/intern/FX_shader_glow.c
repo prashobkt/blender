300152c7d4aca59e43451464c23b9ac7aa3c9143
Antonio Vazquez <blendergit@gmail.com>
2020-02-17 17:42:19 +0100

    GPencil: More tweaks to default parameters for Glow VFX

--- source/blender/shader_fx/intern/FX_shader_glow.c
+++ source/blender/shader_fx/intern/FX_shader_glow.c
@@ -42,7 +42,7 @@ static void initData(ShaderFxData *md)
   ARRAY_SET_ITEMS(gpfx->select_color, 0.0f, 0.0f, 0.0f);
   copy_v2_fl(gpfx->blur, 50.0f);
   gpfx->threshold = 0.1f;
-  gpfx->samples = 16;
+  gpfx->samples = 8;
 }
 
 static void copyData(const ShaderFxData *md, ShaderFxData *target)
