a18d264bf77bac7aef440bee283b53c66f2517ca
Antonio Vazquez <blendergit@gmail.com>
2020-02-23 11:42:54 +0100

    GPencil: Change Tint modifier
    -   Change factor increment to 0.01 and precision 2.

--- source/blender/makesrna/intern/rna_gpencil_modifier.c
+++ source/blender/makesrna/intern/rna_gpencil_modifier.c
@@ -968,7 +968,7 @@ static void rna_def_modifier_gpenciltint(BlenderRNA *brna)
 
   prop = RNA_def_property(srna, "factor", PROP_FLOAT, PROP_NONE);
   RNA_def_property_float_sdna(prop, NULL, "factor");
-  RNA_def_property_ui_range(prop, 0, 2.0, 0.1, 3);
+  RNA_def_property_ui_range(prop, 0, 2.0, 0.01, 2);
   RNA_def_property_ui_text(prop, "Factor", "Factor for mixing color");
   RNA_def_property_update(prop, 0, "rna_GpencilModifier_update");
 
