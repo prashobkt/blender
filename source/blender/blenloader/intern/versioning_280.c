d56b73af864e379d571b6a58a677fa0339c8d2c5
Antonio Vazquez <blendergit@gmail.com>
2020-03-05 18:09:02 +0100

    GPencil: Fix missing Noise curve for old files

--- source/blender/blenloader/intern/versioning_280.c
+++ source/blender/blenloader/intern/versioning_280.c
@@ -4613,7 +4613,7 @@ void blo_do_versions_280(FileData *fd, Library *UNUSED(lib), Main *bmain)
               gpmd->factor_strength = gpmd->factor;
               gpmd->factor_uvs = gpmd->factor;
 
-              if (gpmd->curve_intensity) {
+              if (gpmd->curve_intensity == NULL) {
                 gpmd->curve_intensity = BKE_curvemapping_add(1, 0.0f, 0.0f, 1.0f, 1.0f);
                 if (gpmd->curve_intensity) {
                   BKE_curvemapping_initialize(gpmd->curve_intensity);
