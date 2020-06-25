c8f3e4cc6c055a80561f9a64b7ce164698fca3a3
YimingWu <xp8110@outlook.com>
2020-06-25 17:29:27 +0800

    LANPR: Use only (0,1) for crease threshold.

--- source/blender/makesrna/intern/rna_scene.c
+++ source/blender/makesrna/intern/rna_scene.c
@@ -7406,7 +7406,7 @@ static void rna_def_scene_lanpr(BlenderRNA *brna)
   prop = RNA_def_property(srna, "crease_threshold", PROP_FLOAT, PROP_NONE);
   RNA_def_property_float_default(prop, 0.5f);
   RNA_def_property_ui_text(prop, "Crease Threshold", "cosine value of face angle");
-  RNA_def_property_ui_range(prop, -1.0f, 1.0f, 0.01f, 2);
+  RNA_def_property_ui_range(prop, 0.0f, 1.0f, 0.01f, 2);
   RNA_def_property_flag(prop, PROP_EDITABLE);
   RNA_def_property_update(prop, NC_SCENE, NULL);
 
