86f207f4fdc17fa5ed6b600c1df9f8ed2e44d569
Stefan Werner <stewreo@gmail.com>
2020-07-14 10:56:29 +0200

    macOS: Enabled Embree on arm64.
    This is using the arm port of Embree:
    https://github.com/lighttransport/embree-aarch64
    
    Since that repository does not have releases, I created a fork
    on my personal github account:
    https://github.com/skwerner/embree-aarch64
    
    We could move this to the Blender github account instead.

--- build_files/cmake/Modules/FindEmbree.cmake
+++ build_files/cmake/Modules/FindEmbree.cmake
@@ -41,18 +41,29 @@ FIND_PATH(EMBREE_INCLUDE_DIR
     include
 )
 
+if(APPLE AND "${CMAKE_OSX_ARCHITECTURES}" STREQUAL "arm64")
 
 SET(_embree_FIND_COMPONENTS
-  embree3
-  embree_avx
-  embree_avx2
-  embree_sse42
-  lexers
-  math
-  simd
-  sys
-  tasking
+    embree3
+    lexers
+    math
+    simd
+    sys
+    tasking
 )
+else()
+  SET(_embree_FIND_COMPONENTS
+    embree3
+    embree_avx
+    embree_avx2
+    embree_sse42
+    lexers
+    math
+    simd
+    sys
+    tasking
+  )
+endif()
 
 SET(_embree_LIBRARIES)
 FOREACH(COMPONENT ${_embree_FIND_COMPONENTS})
