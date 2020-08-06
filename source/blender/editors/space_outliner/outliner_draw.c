214223e864217c215f9e07327ef63faf902fbabc
Nathan Craddock <nzcraddock@gmail.com>
2020-08-05 21:30:44 -0600

    Outliner: Use solid collection icon
    Switch to the solid collection icon (already on the sheet) for the
    collection colors. Perhaps use the solid for colored collections, and
    the lines for non-colored?

--- source/blender/editors/space_outliner/outliner_draw.c
+++ source/blender/editors/space_outliner/outliner_draw.c
@@ -2522,7 +2522,7 @@ TreeElementIcon tree_element_get_icon(TreeStoreElem *tselem, TreeElement *te)
           data.drag_parent = (data.drag_id && te->parent) ? TREESTORE(te->parent)->id : NULL;
         }
 
-        data.icon = ICON_GROUP;
+        data.icon = ICON_OUTLINER_COLLECTION;
         break;
       }
       case TSE_GP_LAYER: {
