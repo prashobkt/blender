9a0bf6a4e265fa97dfed16ee0052051244d59a96
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 18:56:22 +0000

    Adding numeric_owner as keyword arguement

--- build_files/buildbot/codesign/base_code_signer.py
+++ build_files/buildbot/codesign/base_code_signer.py
@@ -106,7 +106,7 @@ def extract_files(archive_filepath: Path,
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(tar_file_handle, path=extraction_dir)
